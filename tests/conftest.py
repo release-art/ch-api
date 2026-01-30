1bfde80639841431ea290a05632864e8593bbda2
Ilja Orlovs <VRGhost@users.noreply.github.com>
2026-01-30 12:36:00 +0000

    x

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
-import pytest
 import pathlib
+
+import pytest
+
 
 @pytest.fixture
 def test_resources_path() -> pathlib.Path:
-    out = pathlib.Path(__file__).parent / "resources" / 'tests'
+    out = pathlib.Path(__file__).parent / "resources" / "tests"
     assert out.is_dir(), f"Test resources path does not exist: {out}"
     return out.resolve()
